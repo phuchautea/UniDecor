c2caba947aebd3d8523a0cf0ca01cb555acf62d4
phuchau.tea <phuchau.tea@gmail.com>
2023-06-06 20:11:15 +0700

    Initial commit

---
+++ src/main/java/uni/decor/DecorApplication.java
@@ -0,0 +1,13 @@
+package uni.decor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DecorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DecorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uni/decor/DecorApplicationTests.java
@@ -0,0 +1,13 @@
+package uni.decor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DecorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
