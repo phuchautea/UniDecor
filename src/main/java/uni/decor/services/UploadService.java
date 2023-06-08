54a70f234da849215954d75875e664ad71b0ac2e
CheThanh <chethanh3112@gmail.com>
2023-06-08 14:19:21 +0700

    feat: UploadService ( uniqueName, uploadImage)

---
+++ src/main/java/uni/decor/services/UploadService.java
@@ -0,0 +1,42 @@
+package uni.decor.services;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
+
+@Service
+public class UploadService {
+    @Value("${myapp.image-storage}")
+    private String  imageStorage;
+    public String uploadImage(MultipartFile file) throws IOException {
+        if (!file.isEmpty()) {
+            try {
+
+                String fileName = generateUniqueFileName(file.getOriginalFilename());
+                String uploadDir = imageStorage; // Path cấu hình ở application
+
+                File uploadPath = new File(uploadDir);
+                if (!uploadPath.exists()) {
+                    uploadPath.mkdirs(); // Tạo thư mục lưu trữ nếu không tồn tại
+                }
+
+                File serverFile = new File(uploadPath.getAbsolutePath() + File.separator + fileName);
+                file.transferTo(serverFile);
+
+                // trả về Name của file upload
+                return serverFile.getName();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return  null;
+    }
+    private String generateUniqueFileName(String filename) {
+        String uniqueFileName = UUID.randomUUID().toString() + filename; // tạo uniquename bằng random UUID + filename(original)
+        return uniqueFileName;
+    }
+}
