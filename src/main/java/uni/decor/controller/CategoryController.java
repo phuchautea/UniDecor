57aa74316ba5892b2fc64e4bdcd2953cff4abc53
vuduchoainam <vinhoainam0815@gmail.com>
2023-06-12 09:11:57 +0700

    feat: create CRUD Category, Product

---
+++ src/main/java/uni/decor/controller/CategoryController.java
@@ -0,0 +1,65 @@
+package uni.decor.controller;
+
+
+import uni.decor.entity.Category;
+import uni.decor.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@Controller
+@RequestMapping("/admin/categories")
+public class CategoryController {
+    @Autowired
+    private CategoryService categoryService;
+    @GetMapping
+    public String showAllCategories(Model model) {
+        List<Category> categories = categoryService.getAllCategories();
+        model.addAttribute("categories", categories);
+        return "admin/category/list";
+    }
+    @GetMapping("/add")
+    public String addCategoryForm(Model model) {
+        model.addAttribute("category", new Category());
+        model.addAttribute("categories", categoryService.getAllCategories());
+        return "admin/category/add";
+    }
+    @PostMapping("/add")
+    public String addCategory(@ModelAttribute("category") Category category, BindingResult result, Model model) {
+        if(result.hasErrors()){
+            model.addAttribute("category", category);
+            return "admin/category/add";
+        }
+        categoryService.addCategory(category);
+        return "redirect:/admin/categories";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String editCategoryForm(@PathVariable("id") Long id, Model model) {
+        Category editCategory = categoryService.getCategoryById(id);
+        model.addAttribute("categories", categoryService.getAllCategories());
+        if(editCategory != null) {
+            model.addAttribute("category", editCategory);
+            return "admin/category/edit";
+        }else{
+            return "not-found";
+        }
+    }
+
+    @PostMapping("/edit")
+    public String editCategory(@ModelAttribute("category") Category updatedCategory) {
+        categoryService.updateCategory(updatedCategory);
+        return "redirect:/admin/categories";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteCategoryForm(@PathVariable("id") Long id, Model model) {
+        categoryService.deleteCategory(id);
+        return "redirect:/admin/categories";
+    }
+}
