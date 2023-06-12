af4c764df7c51aada4d0471fe6764af1070f9c45
Le Luc <Letrantanluc2002@gmail.com>
2023-06-12 16:14:58 +0700

    refactor: User

--- src/main/java/uni/decor/entity/User.java
+++ src/main/java/uni/decor/entity/User.java
@@ -1,6 +1,7 @@
 package uni.decor.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
 import lombok.Data;
 
 import java.util.*;
@@ -14,24 +15,33 @@ public class User {
     private Long id;
 
     @Column(name = "email", nullable = false)
+    @NotBlank(message = "Your email is required")
+    @Pattern(regexp = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]+$", message = "Invalid email format")
     private String email;
 
     @Column(name = "password", nullable = false)
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$", message = "Invalid password format")
     private String password;
 
     @Column(name = "phone", nullable = false)
+    @Pattern(regexp = "^0(86|96|97|98|32|33|34|35|36|37|38|39|91|94|83|84|85|81|82|90|93|70|79|77|76|78|92|56|58|99|59|55|87)\\d{7}$", message = "Invalid phone number format")
     private String phone;
 
     @Column(name = "name", nullable = false)
+    @NotBlank(message = "Your name is required")
     private String name;
 
     @Column(name = "birthday", nullable = false)
+    @Past(message = "Birthday must be a date in the past")
+    @Pattern(regexp = "^(0[1-9]|[12][0-9]|3[01])/(0[1-9]|1[0-2])/((19|20)\\d{2})$", message = "Invalid date format. The format should be dd/MM/yyyy")
     private Date birthday;
 
     @Column(name = "gender", nullable = false)
+    @Pattern(regexp = "^(Male|Female|Other)$", message = "Invalid gender. Valid values are Male, Female, or Other")
     private String gender;
 
     @Column(name="verified")
+//    @AssertTrue(message = "User must be verified")
     private boolean verified;
 
 
