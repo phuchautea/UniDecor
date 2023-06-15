33e29064722c486b898f4ddaeb23ba63f1db99c9
vuduchoainam <vinhoainam0815@gmail.com>
2023-06-15 18:53:38 +0700

    refactor: create ManageProductController, fix Product Controller

--- src/main/java/uni/decor/controller/ProductController.java
+++ src/main/java/uni/decor/controller/ProductController.java
@@ -19,61 +19,10 @@ import java.util.List;
 
 
 @Controller
-@RequestMapping("/admin/products")
+@RequestMapping("")
 public class ProductController {
     @Autowired
-    private UploadService uploadService;
-    @Autowired
     private ProductService productService;
-    @Autowired
-    private CategoryService categoryService;
-    @Autowired
-    private ProductVariantService productVariantService;
-    @GetMapping
-    public String showAllProducts(Model model) {
-        List<Product> products = productService.getAllProducts();
-        model.addAttribute("products", products);
-        return "admin/product/list";
-    }
-    @GetMapping("/add")
-    public String addProductForm(Model model) {
-        model.addAttribute("product", new Product());
-        model.addAttribute("categories", categoryService.getAllCategories());
-        return "admin/product/add";
-    }
-    @PostMapping("/add")
-    public String addProduct(@ModelAttribute("product") Product product, BindingResult result, Model model) throws IOException {
-        model.addAttribute("product", product);
-        if(result.hasErrors()){
-            return "admin/product/add";
-        }
-        productService.addProduct(product);
-        return "redirect:/admin/products";
-    }
-
-    @GetMapping("/edit/{id}")
-    public String editProductForm(@PathVariable("id") Long id, Model model) {
-        Product editProduct = productService.getProductById(id);
-        model.addAttribute("categories", categoryService.getAllCategories());
-        if(editProduct != null) {
-            model.addAttribute("product", editProduct);
-            return "admin/product/edit";
-        }else{
-            return "not-found";
-        }
-    }
-
-    @PostMapping("/edit")
-    public String editProduct(@ModelAttribute("product") Product updatedProduct, @RequestParam Long category) {
-        productService.updateProduct(updatedProduct, category);
-        return "redirect:/admin/products";
-    }
-
-    @GetMapping("/delete/{id}")
-    public String deleteProductForm(@PathVariable("id") Long id, Model model) {
-        productService.deleteCProduct(id);
-        return "redirect:/admin/products";
-    }
 
     @GetMapping("/product/{slug}")
     public String getBySlug(@PathVariable("slug") String slug, Model model, HttpServletRequest request) {
