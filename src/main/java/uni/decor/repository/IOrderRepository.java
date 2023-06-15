fbf91651c4d2da4fa6b4380d7008cf7b7ddb561d
phuchau.tea <phuchau.tea@gmail.com>
2023-06-15 16:46:12 +0700

    feat: Order

---
+++ src/main/java/uni/decor/repository/IOrderRepository.java
@@ -0,0 +1,9 @@
+package uni.decor.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import uni.decor.entity.Order;
+
+@Repository
+public interface IOrderRepository extends JpaRepository<Order, Long> {
+}
