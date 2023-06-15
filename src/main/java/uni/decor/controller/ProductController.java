9f91a40aeee93e2d9a5382ca43292c62dffa9df5
vuduchoainam <vinhoainam0815@gmail.com>
2023-06-15 23:36:44 +0700

    feat: Create VNPay

--- src/main/java/uni/decor/controller/ProductController.java
+++ src/main/java/uni/decor/controller/ProductController.java
@@ -15,7 +15,10 @@ import uni.decor.entity.ProductVariant;
 import uni.decor.service.ProductVariantService;
 
 import java.io.IOException;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 
 
 @Controller
@@ -37,4 +40,27 @@ public class ProductController {
             return "redirect:/";
         }
     }
+
+//    @GetMapping("/sort/{sortBy}")
+//    public String sortProducts(@PathVariable String sortBy, Model model)
+//    {
+//        List<Product> products = productService.getAllProducts();
+//        switch (sortBy) {
+//            case "price-ascending":
+//                products.sort(Comparator.comparingDouble(product -> Optional.ofNullable(product.getPrice()).orElse(0.0)));
+//                break;
+//            case "name-ascending":
+//                products.sort(Comparator.comparing(Product::getName, String.CASE_INSENSITIVE_ORDER));
+//                break;
+//            case "name-descending":
+//                products.sort(Comparator.comparing(Product::getName, String.CASE_INSENSITIVE_ORDER));
+//                Collections.reverse(products);
+//                break;
+//            default:
+//                break;
+//
+//        }
+//        model.addAttribute("products", products);
+//        return "product";
+//    }
 }
--- src/main/java/uni/decor/service/VnpayPaymentService.java
+++ src/main/java/uni/decor/service/VnpayPaymentService.java
@@ -1,11 +1,95 @@
 package uni.decor.service;
 
 import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import uni.decor.common.CustomLogger;
+import uni.decor.config.vnpayConfig;
+import uni.decor.encryption.VNPaySecurity;
+import uni.decor.entity.Order;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 @Service
 public class VnpayPaymentService {
-    public String process(int total_price, HttpSession session) {
-        return "";
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Value("${uniDecor.vnpay.vnp_TmnCode}")
+    private String tmnCode;
+    @Value("${uniDecor.vnpay.vnp_BankCode}")
+    private String bankCode;
+    @Value("${uniDecor.vnpay.vnp_ReturnUrl}")
+    private String returnUrl;
+    @Value("${uniDecor.vnpay.vnp_Url}")
+    private String url;
+    @Value("${uniDecor.vnpay.vnp_HashSecret}")
+    private String hashSecret;
+
+
+    public String process(int total_price, HttpSession session) throws UnsupportedEncodingException {
+        Order order = (Order) session.getAttribute("order");
+        String amount = String.valueOf(total_price * 100);
+
+        String vnp_TxnRef = vnpayConfig.getRandomNumber(8); // này là mã đơn hàng
+        String vnp_TmnCode = tmnCode;
+
+        Map<String, String> vnp_Params = new HashMap<>();
+        vnp_Params.put("vnp_Version", "2.1.0");
+        vnp_Params.put("vnp_Command", "pay");
+        vnp_Params.put("vnp_TmnCode", tmnCode);
+        vnp_Params.put("vnp_Amount", String.valueOf(amount));
+        vnp_Params.put("vnp_CurrCode", "VND");
+        vnp_Params.put("vnp_BankCode", bankCode);
+        vnp_Params.put("vnp_TxnRef", vnp_TxnRef);
+        vnp_Params.put("vnp_OrderInfo", "Thanh toan don hang: " + vnp_TxnRef);
+        vnp_Params.put("vnp_Locale", "vn");
+        vnp_Params.put("vnp_ReturnUrl", returnUrl);
+        CustomLogger.info("vnp_Params: "+vnp_Params);
+        Calendar cld = Calendar.getInstance(TimeZone.getTimeZone("Etc/GMT+7"));
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMddHHmmss");
+        String vnp_CreateDate = formatter.format(cld.getTime());
+        vnp_Params.put("vnp_CreateDate", vnp_CreateDate);
+
+        cld.add(Calendar.MINUTE, 15);
+        String vnp_ExpireDate = formatter.format(cld.getTime());
+        vnp_Params.put("vnp_ExpireDate", vnp_ExpireDate);
+
+        List fieldNames = new ArrayList(vnp_Params.keySet());
+        Collections.sort(fieldNames);
+        StringBuilder hashData = new StringBuilder();
+        StringBuilder query = new StringBuilder();
+        Iterator itr = fieldNames.iterator();
+        while (itr.hasNext()) {
+            String fieldName = (String) itr.next();
+            String fieldValue = (String) vnp_Params.get(fieldName);
+            if ((fieldValue != null) && (fieldValue.length() > 0)) {
+                //Build hash data
+                hashData.append(fieldName);
+                hashData.append('=');
+                hashData.append(URLEncoder.encode(fieldValue, StandardCharsets.US_ASCII.toString()));
+                //Build query
+                query.append(URLEncoder.encode(fieldName, StandardCharsets.US_ASCII.toString()));
+                query.append('=');
+                query.append(URLEncoder.encode(fieldValue, StandardCharsets.US_ASCII.toString()));
+                if (itr.hasNext()) {
+                    query.append('&');
+                    hashData.append('&');
+                }
+            }
+        }
+        String queryUrl = query.toString();
+        String vnp_SecureHash = VNPaySecurity.hmacSHA512(hashSecret, hashData.toString());
+        queryUrl += "&vnp_SecureHash=" + vnp_SecureHash;
+        String payUrl = vnpayConfig.vnp_PayUrl + "?" + queryUrl;
+        CustomLogger.info(payUrl);
+        return "redirect:"+payUrl;
+
     }
 }
